5e0e489c71b9962fe7d9876ab95eefc8a523e578
Leo Hajder <leohajder@gmail.com>
2017-09-21 23:48:56 +0200

    Refactor

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-$(function() {
-    function getQuote() {
-        $.ajax({
-            url: "https://api.icndb.com/jokes/random?escape=javascript",
-            method: "GET",
-            success: function(response) {
-                $("#quote-text").text(response.value.joke);
-            },
-            error: function() {
-                $("#out").text("ERROR :(");
-            }
-        });
-    }
+function getQuote() {
+    $.ajax({
+        url: 'https://api.icndb.com/jokes/random?escape=javascript',
+        method: 'GET',
+        success: function (response) {
+            $('#quote-text').text(response.value.joke);
+        },
+        error: function () {
+            $('#out').text('ERROR :(');
+        }
+    });
+}
 
+$(function () {
     getQuote();
 
-    $("#getQuote").click(getQuote);
+    $('#getQuote').click(getQuote);
 
-    $("#tweetOut").click(function() {
-        $(this).attr("href", "https://twitter.com/intent/tweet?text=" + $("#quote-text").text());
+    $('#tweetOut').click(function () {
+        $(this).attr('href', 'https://twitter.com/intent/tweet?text=' + $('#quote-text').text());
     });
 });
